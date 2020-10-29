1ebcca1debb6eeef5ba8d9e2730f3a2bbf2a709e
Derek Stobbs <dstobbs@googlemail.com>
2020-10-29 15:29:58 +0000

    Game Over & Restart
    - Game over screen shown when player loses all health
    - Can click to restart game from game over screen

--- C# Missile Defence/scripts/player.cs
+++ C# Missile Defence/scripts/player.cs
@@ -5,6 +5,7 @@ public class player : Node
 {
     bulletBrain bulletBrain;
     public bool canShoot = true;
+    public bool gameOver = false;
     public int health = 3;
     public int score = 0;
 
@@ -15,10 +16,34 @@ public class player : Node
         updateUI();
     }
 
+    public override void _Input(InputEvent _inputEvent)
+    {
+        if ((_inputEvent.IsActionPressed("click")) && (gameOver == true))
+        {
+            GetTree().ReloadCurrentScene();
+        }
+    }
+
     public void hitPlayer(int damageAmount = 1)
     {
-        health = Math.Max(health - damageAmount, 0);
-        updateUI();
+        if (!gameOver)
+        {
+            health = Math.Max(health - damageAmount, 0);
+            updateUI();
+
+            if (health <= 0)
+            {
+                gameOver = true;
+                canShoot = false;
+
+                var gameOverScreen = (Node2D)GetNode("/root/game/hud/gameOverScreen");
+                gameOverScreen.Visible = true;
+
+                var cannon = (Node2D)GetNode("/root/game/foreground/cannon");
+                bulletBrain.spawnExplosion(cannon.GlobalPosition, "enemy");
+                cannon.QueueFree();
+            }
+        }
     }
 
     public void addScore(int scoreAmount = 1)
